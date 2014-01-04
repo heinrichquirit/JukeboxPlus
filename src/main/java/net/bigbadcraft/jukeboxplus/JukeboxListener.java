d7ae8982a85048eb57b1edca0b8fb76ab0debd9d
heinrichquirit <heinrichquirit@gmail.com>
2014-01-04 03:04:34 -0800

    Re-added economy support and user friendly chat messages, due to my minor errors I had temporarily removed these features.

--- src/main/java/net/bigbadcraft/jukeboxplus/JukeboxListener.java
+++ src/main/java/net/bigbadcraft/jukeboxplus/JukeboxListener.java
@@ -2,6 +2,7 @@ package main.java.net.bigbadcraft.jukeboxplus;
 
 import main.java.net.bigbadcraft.jukeboxplus.IconMenu.OptionClickEvent;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Jukebox;
@@ -44,79 +45,94 @@ public class JukeboxListener implements Listener {
 							String predictedName = event.getName();
 							
 							if (predictedName.equals("13")) {
-								plugin.play(jukebox, Material.GOLD_RECORD); 
+								plugin.play(player, jukebox, Material.GOLD_RECORD); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Cat")) {
-								plugin.play(jukebox, Material.GREEN_RECORD);
+								plugin.play(player, jukebox, Material.GREEN_RECORD); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Blocks")) {
-								plugin.play(jukebox, Material.RECORD_3); 
+								plugin.play(player, jukebox, Material.RECORD_3); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Chirp")) {
-								plugin.play(jukebox, Material.RECORD_4); 
+								plugin.play(player, jukebox, Material.RECORD_4); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Far")) {
-								plugin.play(jukebox, Material.RECORD_5); 
+								plugin.play(player, jukebox, Material.RECORD_5); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Mall")) {
-								plugin.play(jukebox, Material.RECORD_6); 
+								plugin.play(player, jukebox, Material.RECORD_6); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Mellohi")) {
-								plugin.play(jukebox, Material.RECORD_7); 
+								plugin.play(player, jukebox, Material.RECORD_7); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Stal")) {
-								plugin.play(jukebox, Material.RECORD_8); 
+								plugin.play(player, jukebox, Material.RECORD_8); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Strad")) {
-								plugin.play(jukebox, Material.RECORD_9); 
+								plugin.play(player, jukebox, Material.RECORD_9); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Ward")) {
-								plugin.play(jukebox, Material.RECORD_10); 
+								plugin.play(player, jukebox, Material.RECORD_10); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Eleven")) {
-								plugin.play(jukebox, Material.RECORD_11); 
+								plugin.play(player, jukebox, Material.RECORD_11); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Wait")) {
-								plugin.play(jukebox, Material.RECORD_12); 
+								plugin.play(player, jukebox, Material.RECORD_12); 
+								plugin.withdraw(player, jukebox);
 								remove(jukeboxLocation);
 							} else if (predictedName.equals("Play")) {
 								if (!jukebox.isPlaying()) {
 									if (plugin.jukeboxTrack.containsKey(jukeboxLocation)) {
 										jukebox.setPlaying(plugin.jukeboxTrack.get(jukeboxLocation));
+										player.sendMessage(JukeboxPlus.PREFIX + "Playing " + ChatColor.RED + Disc.getEnumName(plugin.jukeboxTrack.get(jukeboxLocation))
+												+ ChatColor.WHITE + ".");
 									}
-								} 
+								}  else {
+									player.sendMessage(JukeboxPlus.PREFIX + "A track is already playing.");
+								}
 							} else if (predictedName.equals("Stop")) {
 								if (jukebox.isPlaying()) {
 									if (!plugin.jukeboxTrack.containsKey(jukeboxLocation)) {
 										plugin.jukeboxTrack.put(jukeboxLocation, jukebox.getPlaying());
+										player.sendMessage(JukeboxPlus.PREFIX + "Stopped playing " + ChatColor.RED + Disc.getEnumName(jukebox.getPlaying()) 
+												+ ChatColor.WHITE + ".");
 									}
 									jukebox.setPlaying(null);
+								} else {
+									player.sendMessage(JukeboxPlus.PREFIX + "There are no tracks to stop.");
 								}
-							}
-							
-							/* 
-							 * Release next version until the assumed loop issue 
-							 * (duplicate messages) is fixed within IconMenu class.
-							 * Problem also resides with this code, where the value keeps looping
-							 * 
-							if (predictedName.equals("Previous")) {
+							} else if (predictedName.equals("Previous")) {
 								if (jukebox.isPlaying()) {
 									int currentTrackId = Disc.getEnumId(jukebox.getPlaying());
-									Bukkit.broadcastMessage(""+currentTrackId);
 									jukebox.setPlaying(Disc.getRecord(currentTrackId != 1 ? currentTrackId - 1 : null));
+									plugin.withdraw(player, jukebox);
 								}
 							} else if (predictedName.equals("Next")) {
 								if (jukebox.isPlaying()) {
 									int currentTrackId = Disc.getEnumId(jukebox.getPlaying());
-									Bukkit.broadcastMessage(""+currentTrackId);
 									jukebox.setPlaying(Disc.getRecord(currentTrackId != 12 ? currentTrackId + 1 : null));
+									plugin.withdraw(player, jukebox);
 								}
 							}
-							*/
+							
+							event.setWillDestroy(true);
+							event.setWillClose(true);
 							
 						}
 					}, plugin)
-					//.setOption(0, new ItemStack(Material.WOOL, 1, (short) 7), "Previous", "Play previous track.")
+					.setOption(0, new ItemStack(Material.WOOL, 1, (short) 7), "Previous", "Play previous track.")
 					.setOption(1, plugin.getDiscItem(Disc.THIRTEEN), "13", "Click to play.")
 					.setOption(2, plugin.getDiscItem(Disc.CAT), "Cat", "Click to play.")
 					.setOption(3, plugin.getDiscItem(Disc.BLOCKS), "Blocks", "Click to play.")
@@ -124,7 +140,7 @@ public class JukeboxListener implements Listener {
 					.setOption(5, plugin.getDiscItem(Disc.FAR), "Far", "Click to play.")
 					.setOption(6, new ItemStack(Material.WOOL, 1, (short) 5), "Play", "Resume track.")
 					.setOption(7, new ItemStack(Material.WOOL, 1, (short) 14), "Stop", "Pause track.")
-					//.setOption(8, new ItemStack(Material.WOOL, 1, (short) 8), "Next", "Play next track.")
+					.setOption(8, new ItemStack(Material.WOOL, 1, (short) 8), "Next", "Play next track.")
 					.setOption(10, plugin.getDiscItem(Disc.MALL), "Mall", "Click to play.")
 					.setOption(11, plugin.getDiscItem(Disc.MELLOHI), "Mellohi", "Click to play.")
 					.setOption(12, plugin.getDiscItem(Disc.STAL), "Stal", "Click to play.")
--- src/main/java/net/bigbadcraft/jukeboxplus/JukeboxPlus.java
+++ src/main/java/net/bigbadcraft/jukeboxplus/JukeboxPlus.java
@@ -1,7 +1,8 @@
 package main.java.net.bigbadcraft.jukeboxplus;
 
-import java.io.IOException;
 import java.util.HashMap;
+
+import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,46 +11,41 @@ import org.bukkit.Material;
 import org.bukkit.block.Jukebox;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JukeboxPlus extends JavaPlugin {
 
+	public static final String PREFIX = "[" + ChatColor.RED + "Jukebox+" + ChatColor.WHITE + "] ";
+	
 	public HashMap<Location, Material> jukeboxTrack = new HashMap<Location, Material>();
 	
-	/*
-	 * Disabled economy due to issues with IconMenu
-	 */
-	//public static Economy economy = null;
+	public static Economy economy = null;
 	
-	//public boolean enableEconomy;
-	//public int costPerDisc;
+	public boolean enableEconomy;
+	public int costPerDisc;
 
 	@Override
 	public void onEnable() {
-		/*
 		saveDefaultConfig();
 		
 		enableEconomy = getConfig().getBoolean("enableEconomy");
 		costPerDisc = getConfig().getInt("costPerDisc");
 		
 		if (enableEconomy) {
-			setupEconomy();
-			info("Economy successfully enabled!");
+			if (Bukkit.getPluginManager().getPlugin("Vault") != null) {
+				setupEconomy();
+				Bukkit.getLogger().info("Economy successfully enabled!");
+			} else {
+				Bukkit.getLogger().severe("Economy support enabled, but did not find Vault to hook into!");
+				Bukkit.getLogger().severe("Please re-adjust your configuration file.");
+			}
 		}
-		*/
 		
 		getServer().getPluginManager().registerEvents(new JukeboxListener(this), this);
 		
-		try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-           Bukkit.getLogger().severe("JukeboxPlus failed to submit data.");
-        }
-		
 	}
 	
-	/*
 	private boolean setupEconomy() {
 		RegisteredServiceProvider<Economy> economyProvider = getServer()
 				.getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
@@ -59,7 +55,6 @@ public class JukeboxPlus extends JavaPlugin {
 
 		return (economy != null);
 	}
-	*/
 	
 	@Override
 	public void onDisable() {
@@ -68,54 +63,44 @@ public class JukeboxPlus extends JavaPlugin {
 	
 	public ItemStack getDiscItem(Disc disc) {
 		switch(Disc.getId(disc)) {
-		case 1:
-			return new ItemStack(Material.GOLD_RECORD, 1);
-		case 2:
-			return new ItemStack(Material.GREEN_RECORD, 1);
-		case 3:
-			return new ItemStack(Material.RECORD_3, 1);
-		case 4:
-			return new ItemStack(Material.RECORD_4, 1);
-		case 5:
-			return new ItemStack(Material.RECORD_5, 1);
-		case 6:
-			return new ItemStack(Material.RECORD_6, 1);
-		case 7:
-			return new ItemStack(Material.RECORD_7, 1);
-		case 8:
-			return new ItemStack(Material.RECORD_8, 1);
-		case 9:
-			return new ItemStack(Material.RECORD_9, 1);
-		case 10:
-			return new ItemStack(Material.RECORD_10, 1);
-		case 11:
-			return new ItemStack(Material.RECORD_11, 1);
-		case 12:
-			return new ItemStack(Material.RECORD_12, 1);
+		case 1: return new ItemStack(Material.GOLD_RECORD, 1);
+		case 2: return new ItemStack(Material.GREEN_RECORD, 1);
+		case 3: return new ItemStack(Material.RECORD_3, 1);
+		case 4: return new ItemStack(Material.RECORD_4, 1);
+		case 5: return new ItemStack(Material.RECORD_5, 1);
+		case 6: return new ItemStack(Material.RECORD_6, 1);
+		case 7: return new ItemStack(Material.RECORD_7, 1);
+		case 8: return new ItemStack(Material.RECORD_8, 1);
+		case 9: return new ItemStack(Material.RECORD_9, 1);
+		case 10: return new ItemStack(Material.RECORD_10, 1);
+		case 11: return new ItemStack(Material.RECORD_11, 1);
+		case 12: return new ItemStack(Material.RECORD_12, 1);
 		default: return null;
 		}
 	}
 	
-	public void info(String msg) {
-		Bukkit.getLogger().info(msg);
+	public void play(Player player, Jukebox jukebox, Material material) {
+		if (enableEconomy) {
+			if (JukeboxPlus.economy.getBalance(player.getName()) >= costPerDisc) {
+				jukebox.setPlaying(material);
+			}
+		} else {
+			jukebox.setPlaying(material);
+		}
 	}
 	
-	public void send(Player player, String discName) {
-		player.sendMessage(ChatColor.GREEN + discName + ChatColor.WHITE + " is now playing.");
-	}
-	
-	public void play(Jukebox jukebox, Material material) {
-		jukebox.setPlaying(material);
-	}
-	
-	/*
-	public void withdraw(Player player) {
+	public void withdraw(Player player, Jukebox box) {
 		if (enableEconomy) {
 			if (JukeboxPlus.economy.getBalance(player.getName()) >= costPerDisc) {
 				JukeboxPlus.economy.withdrawPlayer(player.getName(), costPerDisc);
-				player.sendMessage("[JukeboxPlus] $" + costPerDisc + " has been taken out of your account.");
+				player.sendMessage(PREFIX + "Now playing " + ChatColor.RED + Disc.getEnumName(box.getPlaying()) 
+						+ ChatColor.WHITE + ". $" + costPerDisc + " has been deducted.");
+			} else {
+				player.sendMessage(PREFIX + "You do not have enough money.");
+				box.setPlaying(null);
 			}
+		} else {
+			player.sendMessage(PREFIX + "Now playing " + ChatColor.RED + Disc.getEnumName(box.getPlaying()) + ChatColor.WHITE + ".");
 		}
 	}
-	*/
 }
